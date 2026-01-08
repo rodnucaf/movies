4014d4c6bad3c9c9203891e2c2f298172d43572c
rodnucaf <facundoxrabago@gmail.com>
2026-01-08 15:42:42 -0300

    agregar servicio para DbContext

--- moviesMVC/Program.cs
+++ moviesMVC/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using moviesMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MovieDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 
 var app = builder.Build();
 
