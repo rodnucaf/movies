426452c179a82e57e9ac3d78376a56fc6639854c
rodnucaf <facundoxrabago@gmail.com>
2026-01-08 15:25:40 -0300

    agregar atributo ReviewsUsuario y heredar de IdentityUser

--- moviesMVC/Models/Usuario.cs
+++ moviesMVC/Models/Usuario.cs
@@ -1,11 +1,14 @@
-﻿namespace moviesMVC.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace moviesMVC.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public  DateTime FechaNacimiento { get; set; }
         public  string ImagenUrlPerfil { get; set; }
         public List<Favorito>? PeliculasFavoritas { get; set; }
+        public List<Review>? ReviewsUsuario { get; set; }
     }
 }
