53a79b52122cd299ae9367c162b939ac85d19df2
rodnucaf <facundoxrabago@gmail.com>
2026-01-12 16:17:22 -0300

    agregar UsuarioViewModel

--- moviesMVC/Models/Usuario.cs
+++ moviesMVC/Models/Usuario.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace moviesMVC.Models
@@ -17,4 +18,21 @@ namespace moviesMVC.Models
         public List<Favorito>? PeliculasFavoritas { get; set; }
         public List<Review>? ReviewsUsuario { get; set; }
     }
+
+    public class UsuarioViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [PasswordPropertyText]
+        public string Clave { get; set; }
+        [PasswordPropertyText]
+        public string ConfirmarClave { get; set; }
+    }
+
 }
