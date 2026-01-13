4f173186e6efcbd375fc2087e8f8d7e8afe3a5af
rodnucaf <facundoxrabago@gmail.com>
2026-01-12 23:02:56 -0300

    crear método Register

--- moviesMVC/Controllers/UsuarioController.cs
+++ moviesMVC/Controllers/UsuarioController.cs
@@ -1,12 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using moviesMVC.Models;
 
 namespace moviesMVC.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<Usuario> _userManager;
+        private readonly SignInManager<Usuario> _signInManager;
+        public UsuarioController(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(UsuarioViewModel usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                var nuevoUsuario = new Usuario
+                {
+                    UserName = usuario.Email,
+                    Email = usuario.Email,
+                    Nombre = usuario.Nombre,
+                    Apellido = usuario.Apellido,
+                    ImagenUrlPerfil = "default-profile.png"
+                };
+
+                var resultado = await _userManager.CreateAsync(nuevoUsuario, usuario.Clave);
+
+                if (resultado.Succeeded)
+                {
+                    await _signInManager.SignInAsync(nuevoUsuario, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in resultado.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
+            return View(usuario);
+        }
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string user)
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+
     }
 }
