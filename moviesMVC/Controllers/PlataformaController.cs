f3773f2807cb0e0d911d645c12a10c6960a89c35
rodnucaf <facundoxrabago@gmail.com>
2026-01-20 20:48:44 -0300

    Agregar restricción para usuario admin

--- moviesMVC/Controllers/PlataformaController.cs
+++ moviesMVC/Controllers/PlataformaController.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using moviesMVC.Data;
 using moviesMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace moviesMVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class PlataformaController : Controller
     {
         private readonly MovieDbContext _context;
